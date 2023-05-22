b17a3ba70caa149e6f256b0e07b47b1cbda46166
Hiansdt <vinijao123690@gmail.com>
2023-05-22 17:05:11 -0300

    Alterações no Deisgn e etc.
    - Função para remover itens
    - Mudanças em cores e design

--- lista-de-tarefas/App.js
+++ lista-de-tarefas/App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./components/Header";
 import Main from "./components/Main"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header />
       <View style={{flex: 10, height: 10,}}>
         <Main />
       </View>
@@ -16,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    backgroundColor: 'rgb(195,0,132)',
+    backgroundColor: 'rgb(0,190,256)',
     alignItems: "center",
     justifyContent: "center",
   },
--- lista-de-tarefas/components/Main.js
+++ lista-de-tarefas/components/Main.js
@@ -12,8 +12,7 @@ import React, { Component } from "react";
 export default class Main extends Component {
   state = {
     tarefas: [],
-    nomeTarefa: '.',
-    descTarefa: '',
+    nomeTarefa: "",
   };
 
   addTarefa() {
@@ -22,60 +21,114 @@ export default class Main extends Component {
         ...this.state.tarefas,
         {
           nome: this.state.nomeTarefa,
-          descricao: "blabla",
         },
       ],
       nomeTarefa: "",
     });
   }
 
+  removerTarefa(index) {
+    this.state.tarefas.splice(index, 1)
+    this.setState({
+      tarefas: this.state.tarefas,
+    })
+  }
+
+  ativacaoDeTecla(e) {
+    if (e.key == 'Enter') {
+      this.addTarefa()
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text
-            style={{
-              color: "black",
-              fontSize: 20,
-              fontWeight: 600,
-              padding: 10,
-            }}
-          >
-            To-do
-          </Text>
-        <SafeAreaView style={{flex: 1, flexDirection: 'row'}}>
+          style={{
+            color: "black",
+            fontSize: 20,
+            fontWeight: 600,
+            padding: 10,
+          }}
+        >
+          To-do
+        </Text>
+        <SafeAreaView style={{ flex: 1, flexDirection: "row", padding: 10 }}>
           <TextInput
             placeholder="Digite o nome da tarefa"
-            style={{ height: 30 }}
-            onChangeText={nomeTarefa => this.setState({nomeTarefa})}
-            defaultValue
+            placeholderTextColor="rgb(100,100,100)"
+            style={{
+              padding: 10,
+              borderColor: "rgb(200, 200, 200)",
+              borderWidth: 1,
+              borderRadius: 3,
+            }}
+            clearButtonMode="always"
+            value={this.state.nomeTarefa}
+            onChangeText={(nomeTarefa) => this.setState({ nomeTarefa })}
+            onKeyPress={(e) => this.ativacaoDeTecla(e)}
           />
           <Pressable
-          onPress={() => this.addTarefa()}
-        >
-          <Text
+            onPress={() => this.addTarefa()}
             style={{
-              flex: 1,
-              color: "white",
-              fontSize: 30,
-              fontWeight: 600,
-              padding: 10,
-              backgroundColor: '#a44aff',
-              height: 30,
-              textAlign: "center",
-              alignContent: "center",
-              justifyContent: "center"
+              justifyContent: "center",
+              height: "100%",
+              borderRadius: 2,
             }}
           >
-            +
-          </Text>
-        </Pressable>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 20,
+                fontWeight: 600,
+                width: 30,
+                backgroundColor: "#a44aff",
+                textAlign: "center",
+                borderRadius: 2,
+              }}
+            >
+              +
+            </Text>
+          </Pressable>
         </SafeAreaView>
         <FlatList
+          style={{ marginTop: 10, padding: 10 }}
           data={this.state.tarefas}
           renderItem={({ item }) => (
-            <View>
-              <Text style={{backgroundColor: 'grey'}}>{item.nome}</Text>
-              <Text>{item.descricao}</Text>
+            <View
+              style={{
+                padding: 3,
+                flex: 1,
+                flexDirection: "row",
+                backgroundColor: "rgb(235, 235, 235)",
+              }}
+            >
+              <Text style={{ padding: 5 }}>{item.nome}</Text>
+              <Pressable
+                onPress={() => this.removerTarefa(this.state.tarefas.indexOf(item))}
+                style={{
+                  justifyContent: "center",
+                  height: "100%",
+                  borderRadius: 2,
+                  position: 'absolute',
+                  right: 0,
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 20,
+                    fontWeight: 600,
+                    backgroundColor: "red",
+                    textAlign: "center",
+                    borderRadius: 2,
+                    marginBottom: 5,
+                    width: 20,
+                  }}
+                >
+                  x
+                </Text>
+              </Pressable>
             </View>
           )}
         />
@@ -86,7 +139,8 @@ export default class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginTop: 300,
-  }
-})
+    borderRadius: 3,
+  },
+});
